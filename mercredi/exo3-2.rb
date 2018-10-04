0fc41e32c0a90bb72b2a8536d7a12c9753ab2b52
Tim <timothee.bull@gmail.com>
2018-10-04 10:33:47 +0200

    updated exo3-2.rb

--- mercredi/exo3-2.rb
+++ mercredi/exo3-2.rb
@@ -1,66 +1,34 @@
-# def define_password
-#     puts "Create your password"
-#     print '>'
-#     @user_password = gets.chomp
-# end
+def define_password
+    puts "Create your password"
+    print '>'
+    @user_password = gets.chomp
+end
 
-# def password_verification(password)
-#     puts "Retype your password"
-#     print '>'
-#     password_check = gets.chomp
+def password_verification(password)
+    puts "Retype your password"
+    print '>'
+    password_check = gets.chomp
 
-#     if password_check == password
-#         puts 'Password OK'
-#     else
-#         puts 'Password error, try again'
-#     end
-# end
-
-# def connect(password)
-#     if password == 'Password OK'
-#         puts 'You are online'
-#     else
-#         puts 'Your password is not valid'
-#     end
-# end
-
-# def perform
-#     connect(password_verification(define_password))
-# end
-
-# perform
-
-
-class User
-    def initialize
-        password_verification(password)
-    end
-    def define_password
-        puts "Create your password"
-        print '>'
-        @user_password = gets.chomp
-    end
-
-    def password_verification(password)
-        puts "Retype your password"
-        print '>'
-        password_check = gets.chomp
-
-        if password_check == password
-            puts 'Password OK'
-        else
-            puts 'Password error, try again'
-        end
-    end
-
-    def connect(password)
-        if password == 'Password OK'
-            puts 'You are online'
-        else
-            puts 'Your password is not valid'
-        end
+    if password_check == password
+        puts 'Password OK'
+    else
+        puts 'Password error, try again'
     end
 end
 
-test = User.new
-puts test
+def connect(password)
+    if password == @password
+        puts 'You are online'
+    else
+        puts 'Your password is not valid'
+    end
+end
+
+
+def perform
+    # connect(password_verification(define_password))
+    password_verification(define_password)
+    connect(@password)
+end
+
+perform
