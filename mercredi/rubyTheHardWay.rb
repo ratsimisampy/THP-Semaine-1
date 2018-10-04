3f66d0d2afdce9a5883ea1fb3e576dd1ada6f387
Tim <timothee.bull@gmail.com>
2018-10-04 18:58:36 +0200

    added jeudi/

--- mercredi/rubyTheHardWay.rb
+++ mercredi/rubyTheHardWay.rb
@@ -1,20 +1,66 @@
-def while_training(maxNumber, incrementation)
-    i = 0
-    numbers = []
+# create a mapping of state to abbreviation
+states = {
+  'Oregon' => 'OR',
+  'Florida' => 'FL',
+  'California' => 'CA',
+  'New York' => 'NY',
+  'Michigan' => 'MI'
+}
 
-    for i in (0 .. 6) do
-    puts "At the top i is #{i}"
-    numbers.push(i)
+# create a basic set of states and some cities in them
+cities = {
+  'CA' => 'San Francisco',
+  'MI' => 'Detroit',
+  'FL' => 'Jacksonville'
+}
 
-    # i += incrementation
-    puts "Numbers now: ", numbers
-    puts "At the bottom i is #{i}"
-    end
+# add some more cities
+cities['NY'] = 'New York'
+cities['OR'] = 'Portland'
 
-    puts "The numbers: "
+# puts out some cities
+puts '-' * 10
+puts "NY State has: #{cities['NY']}"
+puts "OR State has: #{cities['OR']}"
 
-    # remember you can write this 2 other ways?
-    numbers.each {|num| puts num }
+# puts some states
+puts '-' * 10
+puts "Michigan's abbreviation is: #{states['Michigan']}"
+puts "Florida's abbreviation is: #{states['Florida']}"
+
+# do it by using the state then cities dict
+puts '-' * 10
+puts "Michigan has: #{cities[states['Michigan']]}"
+puts "Florida has: #{cities[states['Florida']]}"
+
+# puts every state abbreviation
+puts '-' * 10
+states.each do |state, abbrev|
+  puts "#{state} is abbreviated #{abbrev}"
 end
 
-while_training(8, 2) 
+# puts every city in state
+puts '-' * 10
+cities.each do |abbrev, city|
+  puts "#{abbrev} has the city #{city}"
+end
+
+# now do both at the same time
+puts '-' * 10
+states.each do |state, abbrev|
+  city = cities[abbrev]
+  puts "#{state} is abbreviated #{abbrev} and has city #{city}"
+end
+
+puts '-' * 10
+# by default ruby says "nil" when something isn't in there
+state = states['Texas']
+
+if !state
+  puts "Sorry, no Texas."
+end
+
+# default values using ||= with the nil result
+city = cities['TX']
+city ||= 'Does Not Exist'
+puts "The city for the state 'TX' is: #{city}"
